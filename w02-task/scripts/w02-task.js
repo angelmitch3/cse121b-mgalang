d40286dd1b407434fb3375038342cfbfbe8074e3
angelmitch3 <michael_gtmc@yahoo.com>
2024-01-21 06:14:34 -0500

    Updated  Week 2 task script

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -11,17 +11,25 @@ window.onload = function() {
     let imageElement = document.querySelector('#profilePicture');
 
     /* Step 4 - Adding Content */
-    nameElement.innerHTML = `<strong>${fullName}</strong>`;
-    yearElement.textContent = currentYear;
-    imageElement.setAttribute('src', profilePicture); 
-    imageElement.setAttribute('alt', `Profile image of ${fullName}`);
+    if(nameElement) {
+        nameElement.innerHTML = `<strong>${fullName}</strong>`;
+    }
+    if(yearElement) {
+        yearElement.textContent = currentYear;
+    }
+    if(imageElement) {
+        imageElement.setAttribute('src', profilePicture); 
+        imageElement.setAttribute('alt', `Profile image of ${fullName}`);
+    }
 
     /* Step 5 - Array */
     // Declare an array variable to hold your favorite foods
     let favFoods = ['Pizza', 'Pasta', 'Ice Cream'];
 
     // Modify the HTML element with the id of 'food' to display your favorite foods array
-    foodElement.innerHTML = favFoods.join(', ');
+    if(foodElement) {
+        foodElement.innerHTML = favFoods.join(', ');
+    }
 
     // Declare and instantiate a variable to hold another single favorite food item
     let newFavoriteFood = 'Sushi';
@@ -30,17 +38,24 @@ window.onload = function() {
     favFoods.push(newFavoriteFood);
 
     // Append the new array values onto the displayed content of the HTML element with the id of food
-    foodElement.innerHTML += `<br>${favFoods.join(', ')}`;
+    if(foodElement) {
+        foodElement.innerHTML += `<br>${favFoods.join(', ')}`;
+    }
 
     // Remove the first element in the favorite food array
     favFoods.shift();
 
     // Again, append the array output showing the modified array
-    foodElement.innerHTML += `<br>${favFoods.join(', ')}`;
+    if(foodElement) {
+        foodElement.innerHTML += `<br>${favFoods.join(', ')}`;
+    }
 
     // Remove the last element in the favorite food array
     favFoods.pop();
 
     // Append the array output with this final modified favorite foods array
-    foodElement.innerHTML += `<br>${favFoods.join(', ')}`;
+    if(foodElement) {
+        foodElement.innerHTML += `<br>${favFoods.join(', ')}`;
+    }
+    console.log({favFoods});
 }
